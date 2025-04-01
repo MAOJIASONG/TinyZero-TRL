5e656189d03d801bbe65cda615edd105b1e4fe5d
MAOJIASONG <smj812855@163.com>
2025-04-01 16:55:43 +0800

    update sft vlm training scripts

--- scripts/sft_vlm.sh
+++ scripts/sft_vlm.sh
@@ -2,6 +2,6 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
 
 ACCELERATE_LOG_LEVEL=info accelerate launch \
---config_file src/r1-v/configs/zero2.yaml \
-src/r1-v/src/open_r1/sft.py \
+--config_file recipes/accelerate_configs/zero2.yaml \
+src/tinyzero_trl/sft_vlm.py \
 --multirun +vlm=qwen2vl_sft_config.yaml
