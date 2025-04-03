dd318ba1f4080a35dbd5ef0c1ebd182e4a55981a
MAOJIASONG <smj812855@163.com>
2025-04-03 14:55:26 +0800

    update vllm server

--- scripts/grpo_vlm.sh
+++ scripts/grpo_vlm.sh
@@ -1,6 +1,14 @@
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
+# !/bin/bash
 
-ACCELERATE_LOG_LEVEL=info accelerate launch \
+CUDA_VISIBLE_DEVICES=0,1,2,3 trl vllm-serve \
+    --model <model_name> \
+    --tensor_parallel_size 1 \
+    --host 0.0.0.0 \
+    --port 8000 \
+    --gpu_memory_utilization 0.9 \
+    --dtype auto
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 ACCELERATE_LOG_LEVEL=info accelerate launch \
 --main_process_port 29501 \
 --num_processes=3 \
 --config_file recipes/accelerate_configs/zero1.yaml \
